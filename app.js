3256e00f992043f01a6540d92c813e1353d3bfcd
Raul <raul.miraball@gmail.com>
2025-03-06 15:53:44 -0300

    Lógica completa del challenge Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);
+  elementoHTML.innerHTML = texto;
+}
+
+function agregarAmigo() {
+  let inputNombre = document.getElementById("amigo");
+  let nombre = inputNombre.value.trim();
+
+  if (nombre === "") {
+    asignarTextoElemento("#resultado", "Por favor, inserte un nombre");
+    return;
+  }
+
+  listaDeAmigos.push(nombre);
+
+  actualizarListaAmigos();
+
+  inputNombre.value = "";
+
+  asignarTextoElemento("#resultado", `Amigo ${nombre} añadidado a la lista`);
+}
+
+function actualizarListaAmigos() {
+  let listaHTML = document.getElementById("listaAmigos");
+  listaHTML.innerHTML = "";
+
+  for (let i = 0; i < listaDeAmigos.length; i++) {
+    let item = document.createElement("li");
+
+    item.textContent = listaDeAmigos[i];
+
+    listaHTML.appendChild(item);
+  }
+}
+
+function limpiarCaja() {
+  document.querySelector("#amigo").value = "";
+}
+
+function sortearAmigo() {
+  if (listaDeAmigos.length === 0) {
+    asignarTextoElemento("#resultado", "No hay amigos en la lista");
+    return;
+  }
+
+  indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+
+  amigoSorteado = listaDeAmigos[indiceAleatorio];
+
+  asignarTextoElemento("#resultado", `El amigo sorteado es: ${amigoSorteado}`);
+}
